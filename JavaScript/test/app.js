d379595adaece277730ca6e1ac23958798d87ca8
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-06 14:04:24 -0500

    Modify example

--- JavaScript/test/app.js
+++ JavaScript/test/app.js
@@ -1,9 +1,15 @@
+let convert = {};
+
+// Converts Celsius to  fahrenheit
 cToF = function(celsius) {
   if(!Number.isInteger(celsius)) return undefined;
   return celsius * 9 / 5 + 32;
 }
 
+// Converts fahrenheit to celsius
 fToC = function(fahrenheit) {
   if(!Number.isInteger(fahrenheit)) return undefined;
   return (fahrenheit - 32) * 5 / 9;
 }
+
+module.exports = convert;
