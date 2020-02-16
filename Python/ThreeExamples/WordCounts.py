67265e1e39652ad8595012c39a4892445389d052
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-16 10:43:05 -0500

    Update WordCounts.py

--- Python/ThreeExamples/WordCounts.py
+++ Python/ThreeExamples/WordCounts.py
@@ -3,7 +3,8 @@ sys.stderr.write('Hello')
 counts = {} 
 print(sys.stdin)
 for line in sys.stdin: 
-    for word in re.findall(r'[a-z\']+', line.lower()): 
-        counts[word] = counts.get(word, 0) + 1 
-for word, count in sorted(counts.items()): 
-    print(word, count)
+    for word in re.findall(r'[a-z\']+', line.lower()):
+        print(word)    
+        #counts[word] = counts.get(word, 0) + 1 
+#for word, count in sorted(counts.items()): 
+#    print(word, count)
