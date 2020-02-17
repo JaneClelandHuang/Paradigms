fc638fa238ade46a0d779ec177194a9a887cc233
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-17 07:16:43 -0500

    Update ClassExample1.py

--- Python/ClassExample1.py
+++ Python/ClassExample1.py
@@ -2,8 +2,8 @@ class GeekforGeeks:
 	geek = "" 
 
 	# default constructor 
-	#def __init__(self): 
-	#	self.geek = "GeekforGeeks"
+	def __init__(self): 
+		self.geek = "GeekforGeeks"
 
 	# a method for printing data members 
 	def print_Geek(self): 
