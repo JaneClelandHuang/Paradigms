4929ebda00a9eae80c6275df9cd3e7a141b6e46e
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-16 22:38:31 -0500

    Update Example.py

--- Python/gui/Example.py
+++ Python/gui/Example.py
@@ -1,16 +1,11 @@
 from tkinter import *
+ 
 from tkinter import ttk
  
 window = Tk()
  
 window.title("Welcome to LikeGeeks app")
  
-# Label that act as a "Title" something like an H1
-lbl = Label(window, text="Hello", font=("Arial Bold", 50))
- 
-lbl.grid(column=0, row=0)
- 
-# Tabs
 tab_control = ttk.Notebook(window)
  
 tab1 = ttk.Frame(tab_control)
@@ -23,11 +18,11 @@ tab_control.add(tab2, text='Second')
  
 lbl1 = Label(tab1, text= 'label1')
  
-lbl1.grid(column=0, row=2)
+lbl1.grid(column=0, row=0)
  
 lbl2 = Label(tab2, text= 'label2')
  
-lbl2.grid(column=0, row=2)
+lbl2.grid(column=0, row=0)
  
 tab_control.pack(expand=1, fill='both')
  
