454bc78eb71feb8b1b74f2cfb3700da078f324fa
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-18 08:12:58 -0500

    Update Example.py

--- Python/gui/Example.py
+++ Python/gui/Example.py
@@ -1,29 +1,22 @@
-from tkinter import *
- 
-from tkinter import ttk
- 
-window = Tk()
- 
-window.title("Welcome to LikeGeeks app")
- 
-tab_control = ttk.Notebook(window)
- 
-tab1 = ttk.Frame(tab_control)
- 
-tab2 = ttk.Frame(tab_control)
- 
-tab_control.add(tab1, text='First')
- 
-tab_control.add(tab2, text='Second')
- 
-lbl1 = Label(tab1, text= 'label1')
- 
-lbl1.grid(column=0, row=0)
- 
-lbl2 = Label(tab2, text= 'label2')
- 
-lbl2.grid(column=0, row=0)
- 
-tab_control.pack(expand=1, fill='both')
- 
-window.mainloop()
+from tkinter import Tk, Label, Button
+
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
+
+        self.label = Label(master, text="This is our first GUI!")
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def greet(self):
+        print("Greetings!")
+
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
