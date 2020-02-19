1467df33fb0af7b746db1f006e9026063d931d1f
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-19 07:35:26 -0500

    tkinter example files uploaded

--- Python/gui/tkinter3.py
+++ Python/gui/tkinter3.py
@@ -6,30 +6,23 @@ canvas.create_oval(100, 100, 100, 100, fill="red")
 
 class MouseMover():
   def __init__(self):
-    self.item = 0; self.previous = (0, 0)
 
   def select(self, event):
     widget = event.widget # Get handle to canvas 
     # Convert screen coordinates to canvas coordinates
     xc = widget.canvasx(event.x); 
     yc = widget.canvasx(event.y)
-    self.item = widget.find_closest(xc, yc)[0] # ID for closest
-    self.previous = (xc, yc)
     print((xc, yc, self.item))
-
-  def drag(self, event):
-    widget = event.widget
-    xc = widget.canvasx(event.x); 
-    yc = widget.canvasx(event.y)
-    canvas.move(self.item, xc-self.previous[0], yc-self.previous[1])
-    self.previous = (xc, yc)
-
-
-# Get an instance of the MouseMover object
-mm = MouseMover()
+	
+  def print_event(self, event): 
+    position = "(x={}, y={})".format(event.x, event.y) 
+    print(event.type, "event", position) 
 
 # Bind mouse events to methods (could also be in the constructor)
-canvas.bind("<Button-1>", mm.select)
-canvas.bind("<B1-Motion>", mm.drag)
+canvas.bind("<Button-1>", self.print_event)
+canvas.bind("<B1-Motion>", self.print_event)
+canvas.bind("<Double-Button-1>", self.print_event) 
+canvas.bind("<ButtonRelease-1>", self.print_event) 
+canvas.bind("<B1-Motion>", self.print_event) 
 
 root.mainloop()
