ca4889006de84fb7ce20d2abc48e372dd22bad6b
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-18 20:52:11 -0500

    Create mousemover2.py

---
+++ Python/gui/mousemover2.py
@@ -0,0 +1,35 @@
+from tkinter import * 
+root = Tk()
+canvas = Canvas(root, width=400, height=200)
+canvas.pack()
+canvas.create_oval(10, 10, 110, 60, fill="grey")
+
+class MouseMover():
+  def __init__(self):
+    self.item = 0; self.previous = (0, 0)
+
+  def select(self, event):
+    widget = event.widget # Get handle to canvas 
+    # Convert screen coordinates to canvas coordinates
+    xc = widget.canvasx(event.x); 
+	yc = widget.canvasx(event.y)
+    self.item = widget.find_closest(xc, yc)[0] # ID for closest
+    self.previous = (xc, yc)
+    print((xc, yc, self.item))
+
+  def drag(self, event):
+    widget = event.widget
+    xc = widget.canvasx(event.x); 
+	yc = widget.canvasx(event.y)
+    canvas.move(self.item, xc-self.previous[0], yc-self.previous[1])
+    self.previous = (xc, yc)
+
+
+# Get an instance of the MouseMover object
+mm = MouseMover()
+
+# Bind mouse events to methods (could also be in the constructor)
+canvas.bind("<Button-1>", mm.select)
+canvas.bind("<B1-Motion>", mm.drag)
+
+root.mainloop()
