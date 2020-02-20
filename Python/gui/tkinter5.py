b0103cfb5741814e096de8bf3272af99178599a8
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-20 15:12:37 -0500

    Update tkinter5.py

--- Python/gui/tkinter5.py
+++ Python/gui/tkinter5.py
@@ -27,7 +27,9 @@ class MouseMover():
             self.previous = (xc, yc)
 		
     def setSelectedShape(self,xCoord,yCoord,coords):
-        print("HELLO" + xCoord + " " + coords[2])
+        print("HELLO")
+        print(xCoord)
+        print(coords[2])
         #if (xCoord < coords[2] and xCoord > coords[0])# and yCoord < coords[3] and yCoord > coords[1])
         #    self.shape = self.rectangle
         #else:
