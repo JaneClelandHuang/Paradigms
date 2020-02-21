367a8be00d7de35c35c063bd70df85706944f10e
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-20 19:59:56 -0500

    Update tkinter6.py

--- Python/gui/tkinter6.py
+++ Python/gui/tkinter6.py
@@ -2,13 +2,6 @@ import tkinter as tk
 
 app = tk.Tk()
 app.geometry("200x100")
-
-def callback(event):
-    #label["text"] = "You pressed Enter"
-    print("You pressed Enter")
-app.bind('<Return>', callback)
-
-label = tk.Label(app, text="")
-label.pack()
+app.bind('<Return>', print("Hello"))
 
 app.mainloop()
