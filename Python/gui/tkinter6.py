48b50214b1be995e7e1219076fea895d06e056e8
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-20 19:46:55 -0500

    Update tkinter6.py

--- Python/gui/tkinter6.py
+++ Python/gui/tkinter6.py
@@ -7,12 +7,12 @@ label1 = Label(root, text=prompt, width=len(prompt), bg='yellow')
 label1.pack()
 def key(event):
     if event.char == event.keysym:
-        print('Normal Key %r' % event.char)
+        msg = 'Normal Key %r' % event.char
     elif len(event.char) == 1:
         msg = 'Punctuation Key %r (%r)' % (event.keysym, event.char)
     else:
         msg = 'Special Key %r' % event.keysym
-    label1.config(text=msg)
+label1.config(text=msg)
 root.bind_all('<Key>', key)
 root.mainloop()
 
