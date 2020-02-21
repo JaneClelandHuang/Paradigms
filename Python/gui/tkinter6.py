7d19ea00bdc3585cd25ce9a07d0eef37290d5d4b
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-20 20:01:43 -0500

    Update tkinter6.py

--- Python/gui/tkinter6.py
+++ Python/gui/tkinter6.py
@@ -1,7 +1,16 @@
 import tkinter as tk
 
-app = tk.Tk()
-app.geometry("200x100")
-app.bind('<Return>', print("Hello"))
+class app(tk.Frame):
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("300x200")
+        self.label = tk.Label(self.root, text="")
+        self.label.pack()
+        self.root.bind('<Return>', self.callback)
+        self.root.mainloop()
 
-app.mainloop()
+
+    def callback(self, event):
+        self.label["text"] = "You pressed {}".format(event.keysym)
+    
+app()
