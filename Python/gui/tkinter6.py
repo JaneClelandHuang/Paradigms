53851ab924d625d3ad61c9fc9e1c365ceacdbf3b
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-20 19:50:36 -0500

    Update tkinter6.py

--- Python/gui/tkinter6.py
+++ Python/gui/tkinter6.py
@@ -1,18 +1,43 @@
-# bind and show a key press event with Tkinter
-# tested with Python24      vegaseat     20nov2006
-from tkinter import * 
-root = Tk()
-prompt = '      Press any key      '
-label1 = Label(root, text=prompt, width=len(prompt), bg='yellow')
-label1.pack()
-def key(event):
-    if event.char == event.keysym:
-        msg = 'Normal Key %r' % event.char
-    elif len(event.char) == 1:
-        msg = 'Punctuation Key %r (%r)' % (event.keysym, event.char)
-    else:
-        msg = 'Special Key %r' % event.keysym
-label1.config(text=msg)
-root.bind_all('<Key>', key)
-root.mainloop()
+import tkinter as tk
 
+root = tk.Tk()
+
+has_prev_key_release = None
+
+'''
+When holding a key down, multiple key press and key release events are fired in
+succession. Debouncing is implemented in order to squash these repeated events
+and know when the "real" KeyRelease and KeyPress events happen.
+'''
+
+def on_key_release(event):
+    global has_prev_key_release
+    has_prev_key_release = None
+    print "on_key_release", repr(event.char)
+
+def on_key_press(event):
+    print "on_key_press", repr(event.char)
+
+def on_key_release_repeat(event):
+    global has_prev_key_release
+    has_prev_key_release = root.after_idle(on_key_release, event)
+    print "on_key_release_repeat", repr(event.char)
+
+def on_key_press_repeat(event):
+    global has_prev_key_release
+    if has_prev_key_release:
+        root.after_cancel(has_prev_key_release)
+        has_prev_key_release = None
+        print "on_key_press_repeat", repr(event.char)
+    else:
+        on_key_press(event)
+
+frame = tkinter.Frame(root, width=100, height=100)
+frame.bind("<KeyRelease-a>", on_key_release_repeat)
+frame.bind("<KeyPress-a>", on_key_press_repeat)
+frame.bind("<KeyRelease-s>", on_key_release_repeat)
+frame.bind("<KeyPress-s>", on_key_press_repeat)
+frame.pack()
+frame.focus_set()
+
+root.mainloop()
