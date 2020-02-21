366da40a177ef6bf5d9211f486fce3cb02ef28e1
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-20 20:09:34 -0500

    Update tkinter6.py

--- Python/gui/tkinter6.py
+++ Python/gui/tkinter6.py
@@ -1,9 +1,20 @@
-import tkinter as tk
+from tkinter import Tk, Label, Button
 
 class app(tk.Frame):
     def __init__(self):
         self.root = tk.Tk()
         self.root.geometry("300x200")
+		
+        master.title("A simple GUI")
+
+        self.label = Label(master, text="This is our first GUI!")
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
         self.label = tk.Label(self.root, text="")
         self.label.pack()
         self.root.bind('<Return>', self.callback)
@@ -13,4 +24,6 @@ class app(tk.Frame):
     def callback(self, event):
         self.label["text"] = "You pressed {}".format(event.keysym)
     
-app()
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
