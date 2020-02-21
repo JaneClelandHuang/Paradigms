3ffa84eb64bda2e5a3ff53c573d7c42065e4ec5b
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-20 20:29:16 -0500

    Update tkinter6.py

--- Python/gui/tkinter6.py
+++ Python/gui/tkinter6.py
@@ -7,13 +7,13 @@ m = 0
 
 def down(e):
     if m == 0:
-        print 'Down\n', e.char, '\n', e
+        print('Down\n', e.char, '\n', e)
         global m
         m = 1
 
 def up(e):
     if m == 1:
-        print 'Up\n', e.char, '\n', e
+        print('Up\n', e.char, '\n', e)
         global m
         m = 0
 
