a5578e1835befba8b577f88084c2de973b0de507
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-20 21:58:27 -0500

    Update tkinter7.py

--- Python/gui/tkinter7.py
+++ Python/gui/tkinter7.py
@@ -8,16 +8,20 @@ def onUpArrowKey(event):
 
 def onDownArrowKey(event): 
     print('Down arrow key pressed')
-          
+
+def onLeftArrowKey(event): 
+    print('Left arrow key pressed')
+         
 tkroot = Tk()
 labelfont = ('courier', 20, 'bold')                
-widget = Label(tkroot, text='Hello bind world')
-widget.config(bg='red', font=labelfont)            
+widget = Label(tkroot, text='Testing arrow keys')
+widget.config(bg='gray', font=labelfont)            
 widget.config(height=5, width=20)                  
 widget.pack(expand=YES, fill=BOTH)
 
 widget.bind('<Up>',onUpArrowKey)   
 widget.bind('<Down>',onDownArrowKey)
+widget.bind('<Left>',onLeftArrowKey)
 widget.focus()                                     
 tkroot.title('Click Me')
 tkroot.mainloop()
