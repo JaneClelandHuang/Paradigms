c93a85da998d7e8836280dd3e236e7c719f901d4
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-21 09:25:51 -0500

    Update tkinter6.py

--- Python/gui/tkinter6.py
+++ Python/gui/tkinter6.py
@@ -8,6 +8,6 @@ w.pack()
 w.create_line(0, 0, 200, 100)
 w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
 
-w.create_rectangle(50, 25, 150, 75, fill="blue")
+w.create_rectangle(50, 25, 100, 75, fill="blue")
 
 mainloop()
