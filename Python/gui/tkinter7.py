6bc012cf66c9bf07362cf3587a839515417b5021
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-20 20:56:53 -0500

    Update tkinter7.py

--- Python/gui/tkinter7.py
+++ Python/gui/tkinter7.py
@@ -3,9 +3,12 @@ from tkinter import *
 def showPosEvent(event):
     print('Widget=%s X=%s Y=%s' % (event.widget, event.x, event.y))
      
-def onArrowKey(event): 
-    print('Got up arrow key press')
-     
+def onUpArrowKey(event): 
+    print('Up arrow key pressed')
+
+def onDownArrowKey(event): 
+    print('Down arrow key pressed')
+          
 tkroot = Tk()
 labelfont = ('courier', 20, 'bold')                
 widget = Label(tkroot, text='Hello bind world')
@@ -13,7 +16,8 @@ widget.config(bg='red', font=labelfont)
 widget.config(height=5, width=20)                  
 widget.pack(expand=YES, fill=BOTH)
 
-widget.bind('<Up>',        onArrowKey)   
+widget.bind('<Up>',onUpArrowKey)   
+widget.bind('<Down>',onDownArrowKey)
 widget.focus()                                     
 tkroot.title('Click Me')
 tkroot.mainloop()
