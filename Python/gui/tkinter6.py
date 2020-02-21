0a2799088ffbe9739698583792a25d02a4e168cd
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-20 19:56:54 -0500

    Update tkinter6.py

--- Python/gui/tkinter6.py
+++ Python/gui/tkinter6.py
@@ -1,43 +1,14 @@
 import tkinter as tk
 
-root = tk.Tk()
+app = tk.Tk()
+app.geometry("200x100")
 
-has_prev_key_release = None
+def callback(event):
+    label["text"] = "You pressed Enter"
 
-'''
-When holding a key down, multiple key press and key release events are fired in
-succession. Debouncing is implemented in order to squash these repeated events
-and know when the "real" KeyRelease and KeyPress events happen.
-'''
+app.bind('<Return>', callback)
 
-def on_key_release(event):
-    global has_prev_key_release
-    has_prev_key_release = None
-    print("on_key_release", repr(event.char))
+label = tk.Label(app, text="")
+label.pack()
 
-def on_key_press(event):
-    print("on_key_press", repr(event.char))
-
-def on_key_release_repeat(event):
-    global has_prev_key_release
-    has_prev_key_release = root.after_idle(on_key_release, event)
-    print("on_key_release_repeat", repr(event.char))
-
-def on_key_press_repeat(event):
-    global has_prev_key_release
-    if has_prev_key_release:
-        root.after_cancel(has_prev_key_release)
-        has_prev_key_release = None
-        print("on_key_press_repeat", repr(event.char))
-    else:
-        on_key_press(event)
-
-frame = tk.Frame(root, width=100, height=100)
-frame.bind("<KeyRelease-a>", on_key_release_repeat)
-frame.bind("<KeyPress-a>", on_key_press_repeat)
-frame.bind("<KeyRelease-s>", on_key_release_repeat)
-frame.bind("<KeyPress-s>", on_key_press_repeat)
-frame.pack()
-frame.focus_set()
-
-root.mainloop()
+app.mainloop()
