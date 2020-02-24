908a54eb9c1253f5043896d59c5f202a7c9c9fbc
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-24 15:54:13 -0500

    Update grid.py

--- Python/temp/grid.py
+++ Python/temp/grid.py
@@ -1,22 +1,22 @@
 import tkinter as tk
 
 def create_grid(event=None):
-    w = 180 # c.winfo_width() # Get current width of canvas
-    h = 360 #c.winfo_height() # Get current height of canvas
+    w = 240 # c.winfo_width() # Get current width of canvas
+    h = 420 #c.winfo_height() # Get current height of canvas
     c.delete('grid_line') # Will only remove the grid_line
 
     # Creates all vertical lines at intevals of 10
     #for i in range(0, w, 10):
-    for i in range(0,190,10):
+    for i in range(0,w+10,10):
         c.create_line([(i, 0), (i, h)], tag='grid_line')
 
     # Creates all horizontal lines at intevals of 10
-    for i in range(0,370,10):
+    for i in range(0,h+10,10):
         c.create_line([(0, i), (w, i)], tag='grid_line')
 
 root = tk.Tk()
 
-c = tk.Canvas(root, height=360, width=180, bg='white')
+c = tk.Canvas(root, height=h, width=w, bg='white')
 c.pack(fill=tk.BOTH, expand=True)
 
 c.bind('<Configure>', create_grid)
