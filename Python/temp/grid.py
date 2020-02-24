b66ab165036c053ae67ef3765325ff1afd7107bc
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-24 16:05:29 -0500

    Update grid.py

--- Python/temp/grid.py
+++ Python/temp/grid.py
@@ -2,23 +2,11 @@ import tkinter as tk
 root = tk.Tk()
 
 class Grid:
+    def __init__(self)#,width=240,height=420):
+	    self.width = 240
+        self.height = 420
 
-    def __init__(self,width=240,height=420):
-	    self.width = width
-        self.height = height
 
-        # Creates all vertical lines at intevals of 10
-        for i in range(0,w+10,10):
-            c.create_line([(i, 0), (i, h)], tag='grid_line')
-
-        # Creates all horizontal lines at intevals of 10
-        for i in range(0,h+10,10):
-            c.create_line([(0, i), (w, i)], tag='grid_line')
-
-        self.c = tk.Canvas(root, height=h, width=w, bg='white')
-        self.c.pack(fill=tk.BOTH, expand=True)
-
-        #c.bind('<Configure>', create_grid)
 
 grid = Grid()
 root.mainloop()
