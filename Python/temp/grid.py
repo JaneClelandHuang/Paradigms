bd1d9dcd6fa47b517cc17ebff572979a589aaee8
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-24 15:59:01 -0500

    Update grid.py

--- Python/temp/grid.py
+++ Python/temp/grid.py
@@ -1,24 +1,25 @@
 import tkinter as tk
-
-def create_grid(event=None):
-    w = 240 # c.winfo_width() # Get current width of canvas
-    h = 420 #c.winfo_height() # Get current height of canvas
-    c.delete('grid_line') # Will only remove the grid_line
-
-    # Creates all vertical lines at intevals of 10
-    #for i in range(0, w, 10):
-    for i in range(0,w+10,10):
-        c.create_line([(i, 0), (i, h)], tag='grid_line')
-
-    # Creates all horizontal lines at intevals of 10
-    for i in range(0,h+10,10):
-        c.create_line([(0, i), (w, i)], tag='grid_line')
-
 root = tk.Tk()
 
-c = tk.Canvas(root, height=h, width=w, bg='white')
-c.pack(fill=tk.BOTH, expand=True)
+class Grid:
 
-c.bind('<Configure>', create_grid)
+    def __init__(self,width=240,height=420):
+	    this.width = width
+        this.height = height
+	    #c.delete('grid_line') # Will only remove the grid_line
 
+        # Creates all vertical lines at intevals of 10
+        for i in range(0,w+10,10):
+            c.create_line([(i, 0), (i, h)], tag='grid_line')
+
+        # Creates all horizontal lines at intevals of 10
+        for i in range(0,h+10,10):
+            c.create_line([(0, i), (w, i)], tag='grid_line')
+
+        this.c = tk.Canvas(root, height=h, width=w, bg='white')
+        this.c.pack(fill=tk.BOTH, expand=True)
+
+        #c.bind('<Configure>', create_grid)
+
+grid = Grid()
 root.mainloop()
