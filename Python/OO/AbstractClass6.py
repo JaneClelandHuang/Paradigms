3e0ac9a5a14fe78a67150b61dcedea59a303d3db
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-24 06:34:41 -0500

    Commit Abstract class examples 4-6

---
+++ Python/OO/AbstractClass6.py
@@ -0,0 +1,18 @@
+# This does the same thing but imports the Abstract Class
+import AbstractClassExample
+
+class DoAdd42(AbstractClassExample):
+
+    def do_something(self):
+        return self.value + 42
+    
+class DoMul42(AbstractClassExample):
+   
+    def do_something(self):
+        return self.value * 42
+    
+x = DoAdd42(10)
+y = DoMul42(10)
+
+print(x.do_something())
+print(y.do_something())
