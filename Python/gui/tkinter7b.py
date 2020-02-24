930d89aa033432e7c7de858a3035076153d209f3
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-23 20:10:16 -0500

    Update tkinter7b.py

--- Python/gui/tkinter7b.py
+++ Python/gui/tkinter7b.py
@@ -1,16 +1,17 @@
 from tkinter import * 
 root = Tk()
-canvas = Canvas(root, width=400, height=400)
-canvas.pack()
+
 
 class keyPresser():  
-    def fred(self,event): 
+    def handle_down_key(self,event):
+        self.canvas.focus_set()    
         print('Down arrow key pressed')
 
-    def __init__(self):       
-        canvas.bind("<Down>", self.fred)
-        canvas.focus()
-        canvas.pack()
+    def __init__(self):   
+        self.canvas = Canvas(root, width=400, height=400)
+        self.canvas.bind("<Down>", self.fred)
+        self.canvas.focus()
+        self.canvas.pack()
  
 mm = keyPresser()
 root.mainloop()
