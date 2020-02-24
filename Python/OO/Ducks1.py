0681b34d4a2b9f54a79e36b30c66e20dfc18885d
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-24 07:12:43 -0500

    Create Ducks1.py

---
+++ Python/OO/Ducks1.py
@@ -0,0 +1,42 @@
+# Python program showing 
+# abstract base class work 
+from abc import ABC, abstractmethod
+
+class Duck(ABC): 
+    def __init__(self, value):
+        self.value = value
+        super().__init__()
+    
+    @abstractmethod
+    def quack(self):
+        pass
+    
+    def fly(self):
+        pass
+
+class MallardDuck(Duck): 
+    def quack(self):
+        print("Quack Quack")
+    def fly(self):
+        print("I'm flying")
+    
+class RedHeadDuck(Duck):
+    def quack(self):
+        print("Quack Quack")
+    def fly(self):
+        print("I'm flying")
+        
+class RubberDucky(Duck):
+    def quack(self):
+        print("Quack Quack")
+    def fly(self):
+        print("I'm flying")
+
+list = []
+list.append(MallardDuck())
+list.append(RedHeadDuck())
+list.append(RubbyDucky())
+
+for duck in list:
+    print (duck.quack(),duck.fly())
+
