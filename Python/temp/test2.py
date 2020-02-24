5c2ae62574e716145711f58bddce379e84178bcd
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-23 19:00:37 -0500

    Create test2.py

---
+++ Python/temp/test2.py
@@ -0,0 +1,34 @@
+# Demonstrates the arrow keys.
+# Click on the widget first to get focus.
+#widget.focus() doesn't work
+
+from tkinter import *
+tkroot = Tk()
+
+class myClass:
+    def showPosEvent(event):
+        print('Widget=%s X=%s Y=%s' % (event.widget, event.x, event.y))
+     
+    def onUpArrowKey(event): 
+        print('Up arrow key pressed')
+
+    def onDownArrowKey(event): 
+        print('Down arrow key pressed')
+
+    def onLeftArrowKey(event): 
+        print('Left arrow key pressed')
+         
+    def __init__(self):    
+        labelfont = ('courier', 20, 'bold')                
+        widget = Label(tkroot, text='Testing arrow keys')
+        widget.config(bg='gray', font=labelfont)            
+        widget.config(height=5, width=20)                  
+        widget.pack(expand=YES, fill=BOTH)
+
+        widget.bind('<Up>',self.onUpArrowKey)   
+        widget.bind('<Down>',self.onDownArrowKey)
+        widget.bind('<Left>',self.onLeftArrowKey)
+        widget.focus()  
+                                
+tkroot.title('Click Me')
+tkroot.mainloop()
