61e884db8787fca5fb8e243154e34aac4e67c879
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-23 19:55:16 -0500

    Update tkinter7b.py

--- Python/gui/tkinter7b.py
+++ Python/gui/tkinter7b.py
@@ -1,42 +1,14 @@
-# Demonstrates the arrow keys.
-# Click on the widget first to get focus.
-#widget.focus() doesn't work
-
 from tkinter import * 
 root = Tk()
-canvas = Canvas(root, width=400, height=400)
-canvas.pack()
 
-class keyPresser():
-     
-    def showPosEvent(event):
-        print('Widget=%s X=%s Y=%s' % (event.widget, event.x, event.y))
-     
-    def onUpArrowKey(event): 
-        print('Up arrow key pressed')
-
-    def onDownArrowKey(event): 
-        canvas.move(self.rectangle,20,20)
+class keyPresser():  
+    def fred(self,event): 
         print('Down arrow key pressed')
 
-    def onLeftArrowKey(event): 
-        print('Left arrow key pressed')
-
-    def print_event(self, event): 
-        print(event.type) 
-
     def __init__(self):       
-        self.width = 20
-        self.height = 20
-        self.rectangle = canvas.create_rectangle( 
-            0, 0, 20, 20, fill = "black")
-        # Bind keypress events to methods
-        canvas.bind("<Up>", self.print_event)# Note the new binding
-        canvas.bind("<Down>", self.print_event) # Another new binding
+        canvas.bind("<Down>", fred)
         canvas.pack() 
  
-# Get an instance of the MouseMover object
 mm = keyPresser()
-
 root.mainloop()
 
