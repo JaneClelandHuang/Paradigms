8cdd48493294daaf6737e1bb893970b9e59ac461
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-23 21:09:55 -0500

    Create AbstractClass1.py

---
+++ Python/OO/AbstractClass1.py
@@ -0,0 +1,32 @@
+# Python program showing 
+# abstract base class work 
+
+class Polygon(): 
+    # abstract method 
+    def noofsides(self): 
+        pass
+
+class Triangle(Polygon): 
+    # overriding abstract method 
+    def noofsides(self): 
+        print("I have 3 sides") 
+
+class Pentagon(Polygon): 
+    # overriding abstract method 
+    def noofsides(self): 
+        print("I have 5 sides") 
+
+class Hexagon(Polygon): 
+    # overriding abstract method 
+    def noofsides(self): 
+        print("I have 6 sides") 
+
+# Driver code 
+triangle = Triangle() 
+triangle.noofsides() 
+
+pentagon = Pentagon() 
+pentagon.noofsides() 
+
+hexagon = Hexagon() 
+hexagon.noofsides() 
