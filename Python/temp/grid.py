033d6f8c0b5a8ab05aebffa56bff2f09f77af7ee
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-24 17:24:17 -0500

    Update grid.py

--- Python/temp/grid.py
+++ Python/temp/grid.py
@@ -7,9 +7,12 @@ canvas.pack()
 class Grid():  
 
     #defining constructor  
-    def __init__(self, height=360, width=240):  
+    def __init__(self, root, canvas, height=360, width=240):  
         self.height = height  
         self.width = width  
+		self.root = root
+		self.canvas = canvas
+		
         # Creates all vertical lines at intevals of 100
         for i in range(0, self.width+10, 10):
             canvas.create_line([(i, 0), (i, self.height)], tag='grid_line')
@@ -17,11 +20,10 @@ class Grid():
         # Creates all horizontal lines at intevals of 100
         for i in range(0, self.height+10, 10):
             canvas.create_line([(0, i), (self.width, i)], tag='grid_line')
-        c = root.Canvas(root, height=1000, width=1000, bg='white')
-        c.pack(fill=tk.BOTH, expand=True)
+  
 
  
     #end of the class definition  
   
 # Create an object of the class  
-grid = Grid()
+grid = Grid(root,canvas)
