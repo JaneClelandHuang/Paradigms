0f6165e59c2cd30288a2254d4391fa56f701de46
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-23 20:16:32 -0500

    Update tkinter7b.py

--- Python/gui/tkinter7b.py
+++ Python/gui/tkinter7b.py
@@ -9,6 +9,8 @@ class keyPresser():
 
     def __init__(self):   
         self.canvas = Canvas(root, width=400, height=400)
+        self.rectangle = canvas.create_rectangle( 
+            0, 0, 20, 20, fill = "black")
         self.canvas.bind("<Down>", self.handle_down_key)
         self.canvas.focus()
         self.canvas.pack()
