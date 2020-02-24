c64625317b2d7b0a64a76d53ba27e0cd68b0719a
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-24 16:09:51 -0500

    Update grid.py

--- Python/temp/grid.py
+++ Python/temp/grid.py
@@ -4,7 +4,7 @@ root = tk.Tk()
 class Grid:
     def __init__(self,width=240,height=360): #,width=240,height=420):
 	    self.width = width
-        self.height=height
+        self.height = height
 
 grid = Grid()
 root.mainloop()
