29e165c58e2b36bfe66ffbd0a9e3cd45f6cd13e7
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-24 16:40:18 -0500

    Update grid.py

--- Python/temp/grid.py
+++ Python/temp/grid.py
@@ -10,14 +10,15 @@ class Grid:
     def __init__(self, height=360, width=240):  
         self.height = height  
         self.width = width  
+    # Creates all vertical lines at intevals of 100
+    for i in range(0, width+10, 10):
+        c.create_line([(i, 0), (i, h)], tag='grid_line')
+
+    # Creates all horizontal lines at intevals of 100
+    for i in range(0, height+10, 10):
+        c.create_line([(0, i), (w, i)], tag='grid_line')
    
-    #defining class methods  
-    def showName(self):  
-        print(self.name)  
-  
-    def showAge(self):  
-        print(self.age)  
-          
+ 
     #end of the class definition  
   
 # Create an object of the class  
