39f4bfd25ad2c914204a2f3478a8f25bc61b94a4
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-25 08:14:43 -0500

    Update AppSetup3.py

--- Python/application/AppSetup3.py
+++ Python/application/AppSetup3.py
@@ -4,13 +4,23 @@ class Grid:
     def __init__(self, master):
         self.master = master
         self.master.title("Lines")
-        self.canvas = tk.Canvas(self.master)
+        #self.canvas = tk.Canvas(self.master)
+        self.height = 360 
+        self.width = 240 
+        canvas = tk.Canvas(self.master, self.width, self.height)
         self.drawGrid()
         self.canvas.pack()
     def drawGrid(self):
-        self.canvas.create_line(15, 25, 200, 25)
-        self.canvas.create_line(300, 35, 300, 200, dash=(4, 2))
-        self.canvas.create_line(55, 85, 155, 85, 105, 180, 55, 85)
+        # Creates all vertical lines at intervals of 100
+        for i in range(0, self.width+10, 10):
+            self.canvas.create_line([(i, 0), (i, self.height)], tag='grid_line')
+
+        # Creates all horizontal lines at intervals of 100
+        for i in range(0, self.height+10, 10):
+            canvas.create_line([(0, i), (self.width, i)], tag='grid_lin
+        #self.canvas.create_line(15, 25, 200, 25)
+        #self.canvas.create_line(300, 35, 300, 200, dash=(4, 2))
+        #self.canvas.create_line(55, 85, 155, 85, 105, 180, 55, 85)
         
 class Window1:
     def __init__(self, master):
