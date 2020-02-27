9c50e96e91e72e2da9f2e6177bf9faa61226cbe3
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-27 14:42:49 -0500

    Update MultipleInheritance1.py

--- Python/MultipleInheritance1.py
+++ Python/MultipleInheritance1.py
@@ -1,15 +1,3 @@
-class B1(B2,B3):
-    def speakX(self):
-        print("B1")
-
-class B2(B4,B5):
-    def speakX(self):
-        print("B2")
-		
-class B3(B6,B7):
-    def speakX(self):
-        print("B3")
-
 class B4:
     def speak(self):
         print("B4")
@@ -25,6 +13,18 @@ class B6:
 class B7:
     def speak(self):
         print("B7")
+		
+class B3(B6,B7):
+    def speakX(self):
+        print("B3")
+		
+class B2(B4,B5):
+        print("B2")
+    def speakX(self):
+		
+class B1(B2,B3):
+    def speakX(self):
+        print("B1")
 		
 b1 = B1()
 b1.speak()
