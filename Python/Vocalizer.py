42f638775fa02492cf2451a1ef2680443b04c26a
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-27 13:45:40 -0500

    updates

---
+++ Python/Vocalizer.py
@@ -0,0 +1,13 @@
+from subprocess import call
+from animals import Cow
+
+class Vocalizer:
+    def vocalize(self):
+        cal(['say',self.name + 'says' + self.sound()])
+		
+class VocalCow(Vocalizer, Cow):
+    pass
+
+c = VocalCow('Bessie')
+print(c.speak())    # speak is inherited from cow
+c.vocalize()        # vocalize is inherited from vocalizer
