4dbdd4465d820f1418aa5ee05048dabcbce90c45
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-02-27 19:46:57 -0500

    Update Vectors.py

--- Python/Vectors.py
+++ Python/Vectors.py
@@ -5,17 +5,17 @@ class Vector:
         self.i = i 
         self.j = j 
 
-def magnitude(self): 
-    return math.sqrt(self.i * self.i + self.j * self.j) 
+    def magnitude(self): 
+        return math.sqrt(self.i * self.i + self.j * self.j) 
 
-def __add__(self, v): 
-   return Vector(self.i + v.i, self.j + v.j) 
+    def __add__(self, v): 
+      return Vector(self.i + v.i, self.j + v.j) 
 
-def __mul__(self, v): 
-    return self.i * v.i + self.j * v.j 
+    def __mul__(self, v): 
+        return self.i * v.i + self.j * v.j 
 
-def __str__(self): 
-    return '<{},{}>'.format(self.i, self.j) 
+    def __str__(self): 
+        return '<{},{}>'.format(self.i, self.j) 
 
 u = Vector(3,4)
 v = Vector(-5,10)
