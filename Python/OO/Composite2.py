15bf96f72ae62b4d478b52e6fcabc380a50f8e5e
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-01 12:38:58 -0500

    Update Composite2.py

--- Python/OO/Composite2.py
+++ Python/OO/Composite2.py
@@ -4,30 +4,30 @@ from CompositePattern import *
 class Book(Composite):
     def __init__(self,bookTitle):
         self.bookTitle = bookTitle
-    def performOperation(self)
+    def performOperation(self):
         print("\n"+self.bookTitle)
         
 class FrontMatter(Leaf):
-    def performOperation(self)
+    def performOperation(self):
         print("Front Matter")
         print("ISBN: XXXXXXX")
 
 class Chapter(Composite):   
     def __init__(self,chapterTitle):
         self.chapterTitle = chapterTitle
-    def performOperation(self)
+    def performOperation(self):
         print("\n" + self.chapterTitle)
         print("Chapter content here")
 
 class Paragraph(Leaf):
-    def performOperation(self)
+    def performOperation(self):
         print("Content for the chapter goes here.")
 
 class Figure(Leaf):
     def __init__(self,caption,imageName):
         self.caption = caption
         self.imageName = imageName
-    def performOperation()
+    def performOperation():
         print(self.caption)
         print(self.imageName + " Image displayed here")    
 
