97af9187a2589675a84c7218bed4047a43a88c3f
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-01 11:45:30 -0500

    Update Composite.py

--- Python/OO/Composite.py
+++ Python/OO/Composite.py
@@ -19,31 +19,42 @@ class FrontMatter(Book):
         print("ISBN: XXXXXXX")
 
 class Chapter(Book):   
-    def title(self,chapterTitle):
-        print("Chapter: " + chapterTitle)
+    def __init__(self,chapterTitle):
+        self.chapterTitle = chapterTitle
+        self.bookTitle = bookTitle
+        self.title()
+    def title(self):
+        print("Chapter: " + self.chapterTitle)
     def content(self):
         print("Chapter content here")
 
 class Paragraph(Chapter):
+    def __init__(self):
+        self.title()
+        self.content()
     def title(self):
         pass
     def content(self):
         print("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
 
 class Figure(Chapter):
-    def title(self,caption):
+    def __init__(self,caption,imageName):
         self.caption = caption
+        self.imageName = imageName()
+        self.title()
+        self.content()
+    def title(self,caption):
         print(self.caption)
-    def content(self,imageName):
-        print(imageName + " Image displayed here")    
+    def content(self):
+        print(self.imageName + " Image displayed here")    
 
 book = Book("Programming Paradigms")
 frontMatter = FrontMatter()
-#chapter1 = Chapter("Java Script")
-#paragraph1 = Paragraph()
-#paragraph2 = Paragraph()
-#figure1 = Figure("Java Script timeline")
-#chapter2 = Chapter("Python")
-#paragraph3 = Paragraph()
-#paragraph4 = Paragraph()
-#figure1 = Figure("Python Timeline")
+chapter1 = Chapter("Java Script")
+paragraph1 = Paragraph()
+paragraph2 = Paragraph()
+figure1 = Figure("Java Script timeline")
+chapter2 = Chapter("Python")
+paragraph3 = Paragraph()
+paragraph4 = Paragraph()
+figure1 = Figure("Python Timeline")
