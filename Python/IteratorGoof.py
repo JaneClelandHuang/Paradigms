761a832d7256be1e7217d7222bba1c2c5305c9d7
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-02 07:04:28 -0500

    Update IteratorGoof.py

--- Python/IteratorGoof.py
+++ Python/IteratorGoof.py
@@ -1,7 +1,5 @@
 class IteratorGoof():
 
-    #Iterator that counts upward forever.
-
     def __init__(self):
         self.animals = ["cat","dog","turkey","camel","giraffe"]
         self.position = 0
@@ -9,13 +7,21 @@ class IteratorGoof():
     def __iter__(self):
         return self
 
-    def __next__(self):
+    def __nextQ__(self):
         if (self.position < len(self.animals)):
            animal = self.animals[self.position]
            self.position= self.position+1
         else:
            animal = None
         return animal
+        
+    def __next__(self):
+        try:
+           animal = self.animals[self.position]
+           self.position= self.position+1
+        except StopIteration:
+           print("No more items")
+           return None
 
 ig = IteratorGoof()
 print(next(ig))
