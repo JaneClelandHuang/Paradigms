08c36fdaac04d4f5cd4a5c95b8ba9190fc3adfd6
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-03 20:43:27 -0500

    Update MagicNumbers2.rb

--- Ruby/ThreeExamples/MagicNumbers2.rb
+++ Ruby/ThreeExamples/MagicNumbers2.rb
@@ -1,6 +1,6 @@
-10.upto(40) do |c| 
-    10.upto(c - 1) do |b| 
-        10.upto(b - 1) do |a| 
+upto(40) do |c| 
+    upto(c - 1) do |b| 
+        upto(b - 1) do |a| 
             puts "#{a}, #{b}, #{c}" if a * a + b * b == c * c 
         end 
     end 
