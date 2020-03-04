d04ded633c2a6d61ce8fca228847ba3681e07f89
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-03 20:41:13 -0500

    Update MagicNumbers2.rb

--- Ruby/ThreeExamples/MagicNumbers2.rb
+++ Ruby/ThreeExamples/MagicNumbers2.rb
@@ -1,6 +1,6 @@
-fakeObject.upto(40) do |c| 
-    fakeObject.upto(c - 1) do |b| 
-        fakeObject.upto(b - 1) do |a| 
+0.upto(40) do |c| 
+    0.upto(c - 1) do |b| 
+        0.upto(b - 1) do |a| 
             puts "#{a}, #{b}, #{c}" if a * a + b * b == c * c 
         end 
     end 
