f4182f1259c201fa9340bc79379f0abc7f24565e
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-13 13:25:17 -0400

    Update MagicNumbers.java

--- Java/ThreeExamples/MagicNumbers.java
+++ Java/ThreeExamples/MagicNumbers.java
@@ -1,12 +1,10 @@
-public class Tripleapp{
+public class TripleApp{
 	public static void main(String[] args) {
-			for (int c = 1; c <= 40; c++){
-				for (int b = 1; b < c; b++) {
-					for (int a = 1; a < b; a++) {
-						if (a * a + b * b == c * c) {
-							//system.out.printf("%d, %d, &d\n" a,b,c);
-							printline("Hello");
-						}
+		for (int c = 1; c <= 40; c++){
+			for (int b = 1; b < c; b++) {
+				for (int a = 1; a < b; a++) {
+					if (a * a + b * b == c * c) {
+						System.out.printf("%d, %d, &d\n", a,b,c);
 					}
 				}
 			}
