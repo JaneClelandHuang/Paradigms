ca352f17266853821f7f720a6b934d2f2dd5fd3c
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-15 09:58:19 -0400

    Update ConversionDemo.java

--- Java/ConversionDemo.java
+++ Java/ConversionDemo.java
@@ -3,12 +3,15 @@ public class ConversionDemo {
         long a = 1;
         long b = 2;
         long min = -9223372036854775808L;
-        long max = 9223372036854775807L;//Inclusive
+        long max = 9223372036854775807L;
+		long testLongVar = 9223372036854775700L;
+		int testIntVar = 10;
 
         System.out.println(a);
         System.out.println(b);
         System.out.println(a + b);
         System.out.println(min);
         System.out.println(max);
+		System.out.println(testLongVar + testIntVar);
     }
 }
