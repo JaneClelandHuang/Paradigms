037f5ac89b4157d4a2a1907aabaed1b2c09c1623
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-15 10:22:12 -0400

    Update CircleApp.java

--- Java/CircleApp.java
+++ Java/CircleApp.java
@@ -4,5 +4,10 @@ public class CircleApp {
 		Circle c2 = Circle.create(2,8,4);
 		assert c1.area() == Math.PI && c2.area() ==100*Math.PI;
 		assert Circle.area(2) == 4*Math.PI;
+		
+		// Extra output
+		System.out.println(c1.area());
+		System.out.println(c2.area());
+		System.out.println(Circle.area(2));
 	}
 }
