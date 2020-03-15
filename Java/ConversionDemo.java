0ac4da95712b36e263c94edf545299540d8b190c
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-15 09:07:11 -0400

    Update ConversionDemo.java

--- Java/ConversionDemo.java
+++ Java/ConversionDemo.java
@@ -2,8 +2,8 @@ public class ConversionDemo {
      public static void main(String[] args) {
         long a = 1;
         long b = 2;
-        long min = -9223372036854775808;
-        long max = 9223372036854775807;//Inclusive
+        long min = -9223372036854775808L;
+        long max = 9223372036854775807L;//Inclusive
 
         System.out.println(a);
         System.out.println(b);
