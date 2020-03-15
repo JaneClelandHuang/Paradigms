a0bad10f1bb83816507615d6cf32368f5c268db3
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-15 09:01:45 -0400

    Update ConversionDemo.java

--- Java/ConversionDemo.java
+++ Java/ConversionDemo.java
@@ -1,7 +1,7 @@
 public class ConversionDemo {	
     public static void main(String[] args) {
 		int myInt = 10;
-		long myLong = 18446744073709500000;
+		long myLong = 18446744073709500000L;
 		System.out.println(myLong + myInt);
 		System.out.println(myInt + myLong);       
     }
