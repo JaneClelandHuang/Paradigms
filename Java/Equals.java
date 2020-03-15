ae7f9a6095cab6118d7061e8802d3e1dbeaf2769
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-15 08:19:43 -0400

    Update Equals.java

--- Java/Equals.java
+++ Java/Equals.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 public class EqualsDemo {
 	public static void main(String[] args) {
 		LocalDate d1 = LocalDate.of(2020,3,15);
-		LocalDate d1 = LocalDate.of(2020,3,15);
+		LocalDate d2 = LocalDate.of(2020,3,15);
 		assert !(d1 == d2) && d1.equals(d2);
 		
 		// Extra output!
