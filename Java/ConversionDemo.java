727b7bddb5f46f6edcdf1cf8a4ccab71896c37c7
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-15 09:03:32 -0400

    Update ConversionDemo.java

--- Java/ConversionDemo.java
+++ Java/ConversionDemo.java
@@ -1,8 +1,14 @@
-public class ConversionDemo {	
-    public static void main(String[] args) {
-		int myInt = 10;
-		long myLong = 18446744073709500000L;
-		System.out.println(myLong + myInt);
-		System.out.println(myInt + myLong);       
+public class ConversionDemo {
+     public static void main(String[] args) {
+        long a = 1;
+        long b = 2;
+        long min = -9223372036854775808;
+        long max = 9223372036854775807;//Inclusive
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a + b);
+        System.out.println(min);
+        System.out.println(max);
     }
 }
