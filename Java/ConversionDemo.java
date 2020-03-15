40e673c2781e646c8a465ab126c09b3c091eec44
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-15 08:57:21 -0400

    Create ConversionDemo.java

---
+++ Java/ConversionDemo.java
@@ -0,0 +1,7 @@
+public class ConversionDemo {	
+    public static void main(String[] args) {
+		int myInt = 10;
+		long myLong = 14567939494929;
+		System.out.println(myInt + myLong);       
+    }
+}
