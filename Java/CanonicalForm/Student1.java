ddcf78ed77e29fe08b469190615acfccccbc24ee
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-03-19 16:47:11 -0400

    fixed names

--- Java/CanonicalForm/Student1.java
+++ Java/CanonicalForm/Student1.java
@@ -1,17 +1,17 @@
-class Student1{  
+public class Student1{  
     int rollno;  
     String name;  
     String city;  
       
-    Student(int rollno, String name, String city){  
+    public Student1(int rollno, String name, String city){  
 		this.rollno=rollno;  
 		this.name=name;  
 		this.city=city;  
 	}  
       
 	public static void main(String args[]){  
-		Student s1=new Student(101,"Raj","lucknow");  
-		Student s2=new Student(102,"Vijay","ghaziabad");  
+		Student1 s1=new Student(101,"Raj","lucknow");  
+		Student1 s2=new Student(102,"Vijay","ghaziabad");  
          
 		System.out.println(s1);//compiler writes here s1.toString()  
 		System.out.println(s2);//compiler writes here s2.toString()      }  
--- Java/CanonicalForm/Student2.java
+++ Java/CanonicalForm/Student2.java
@@ -1,9 +1,9 @@
-class Student1{  
+public class Student2{  
     int rollno;  
     String name;  
     String city;  
       
-    Student(int rollno, String name, String city){  
+    public Student2(int rollno, String name, String city){  
 		this.rollno=rollno;  
 		this.name=name;  
 		this.city=city;  
@@ -14,8 +14,8 @@ class Student1{
 	}  
       
 	public static void main(String args[]){  
-		Student s1=new Student(101,"Raj","lucknow");  
-		Student s2=new Student(102,"Vijay","ghaziabad");  
+		Student2 s1=new Student(101,"Raj","lucknow");  
+		Student2 s2=new Student(102,"Vijay","ghaziabad");  
          
 		System.out.println(s1);//compiler writes here s1.toString()  
 		System.out.println(s2);//compiler writes here s2.toString()      }  
