108385d0e5f7c6eac415cb4c89935c73978ac2d4
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-04-03 14:31:54 -0400

    Update PrimePrinter2.java

--- Java/Threading/PrimePrinter2.java
+++ Java/Threading/PrimePrinter2.java
@@ -1,6 +1,7 @@
 public class PrimePrinter2 {
 	public static void main(String[] args) {
 		for (int j = 1; j < 10; j++) {
+			String threadName = "Thread-" + ((Integer)i).toString();
 			new Thread(() -> {
 				int primeSum = 0;
 				synchronized (System.out) {
