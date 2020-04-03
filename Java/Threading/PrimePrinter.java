55abf22c631b8242c6820acaea7755106eb6c548
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-04-03 14:14:36 -0400

    Update PrimePrinter.java

--- Java/Threading/PrimePrinter.java
+++ Java/Threading/PrimePrinter.java
@@ -1,6 +1,6 @@
 public class PrimePrinter {
 	public static void main(String[] args) {
-		for (int i = 1; i <= 100; i++) {
+		for (int i = 1; i <= 10000; i++) {
 			final int candidate = i;
 			new Thread(() -> {
 				for (int divisor = 2; divisor <= candidate; divisor++){
