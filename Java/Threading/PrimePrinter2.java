b2c51b6cc96bc00ddfeead19a244a388e984910b
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-04-03 14:20:24 -0400

    Create PrimePrinter2.java

---
+++ Java/Threading/PrimePrinter2.java
@@ -0,0 +1,20 @@
+public class PrimePrinter {
+	public static void main(String[] args) {
+		for (int i = 1; i < 10; i++) {
+			new Thread(() -> {
+				for (int i = 1; i <= 100000; i++) {
+					final int candidate = i;
+					for (int divisor = 2; divisor <= candidate; divisor++){
+						if (divisor == candidate) {
+							synchronized (System.out) {
+								System.out.print(" " + candidate);
+							}
+						} else if (candidate % divisor == 0) {
+							break;
+						}
+					}
+				}).start();
+			}
+		}
+	}
+}
