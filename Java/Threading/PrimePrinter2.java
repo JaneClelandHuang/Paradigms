c9497d9c2d2d981441da764e5bf255a0d2c46ed6
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-04-03 14:26:45 -0400

    Update PrimePrinter2.java

--- Java/Threading/PrimePrinter2.java
+++ Java/Threading/PrimePrinter2.java
@@ -1,18 +1,24 @@
 public class PrimePrinter2 {
 	public static void main(String[] args) {
 		for (int j = 1; j < 10; j++) {
-			new Thread(() -> {
+			new Thread(("Thread-" + ((Integer)i).toString()) -> {
+				int primeSum = 0;
+				synchronized (System.out) {
+					System.out.println("Starting " + Thread.currentThread().getName());
+				}
 				for (int i = 1; i <= 100000; i++) {
 					final int candidate = i;
 					for (int divisor = 2; divisor <= candidate; divisor++){
 						if (divisor == candidate) {
-							synchronized (System.out) {
-								System.out.print(" " + candidate);
+							primeSum += candidate;
 							}
 						} else if (candidate % divisor == 0) {
 							break;
 						}
 					}
+				}
+				synchronized (System.out) {
+					System.out.println("Ending " + Thread.currentThread().getName() + " " + primeSum);
 				}
 			}).start();
 		}
