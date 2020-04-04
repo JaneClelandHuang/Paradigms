81e10a4a0e7698f9a305ae233c5cd004fc702988
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-04-04 10:23:06 -0400

    Update PrimePrinter.java

--- Java/Threading/PrimePrinter.java
+++ Java/Threading/PrimePrinter.java
@@ -1,10 +1,12 @@
 public class PrimePrinter {
 	public static void main(String[] args) {
+		Random rand = new Random();
 		for (int i = 1; i <= 100; i++) {
 			final int candidate = i;
 			new Thread(() -> {
 				for (int divisor = 2; divisor <= candidate; divisor++){
 					if (divisor == candidate) {
+						Thread.sleep(rand.nextInt(2000));
 						synchronized (System.out) {
 							System.out.print(" " + candidate);
 						}
