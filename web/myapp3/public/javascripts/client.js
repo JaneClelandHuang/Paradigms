9f1b8972e7490f41cb23163414b644096fddfa9b
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2020-04-21 20:47:12 -0400

    Final version

--- web/myapp3/public/javascripts/client.js
+++ web/myapp3/public/javascripts/client.js
@@ -1,37 +1,37 @@
 console.log('Client-side code running');
 
+const zooButton = document.getElementById('zooButton');
+zooButton.addEventListener('click', function(e) {
+  console.log('Zoo button was clicked');
+  //location.href = "http://localhost:3000/"; 
+  document.getElementById("AnimalImage").src ="images/zoo.jpg"
+});
+
 const camelButton = document.getElementById('camelButton');
 camelButton.addEventListener('click', function(e) {
   console.log('Camel button was clicked');
-  location.href = "http://localhost:3000/camel"; 
+  //location.href = "http://localhost:3000/camel"; 
+  document.getElementById("AnimalImage").src ="images/camel.jpg"
 });
 
 const monkeyButton = document.getElementById('monkeyButton');
 monkeyButton.addEventListener('click', function(e) {
   console.log('Monkey button was clicked');
-  location.href = "http://localhost:3000/monkey"; 
+  //location.href = "http://localhost:3000/monkey"; 
+  document.getElementById("AnimalImage").src ="images/monkey.jpg"
 });
 
 const crocButton = document.getElementById('crocButton');
 crocButton.addEventListener('click', function(e) {
   console.log('Crocodile button was clicked');
-  location.href = "http://localhost:3000/crocodile"; 
+  //location.href = "http://localhost:3000/crocodile"; 
+  document.getElementById("AnimalImage").src ="images/crocodile.jpg"
 });
 
 const cowButton = document.getElementById('cowButton');
 cowButton.addEventListener('click', function(e) {
   console.log('Cow button was clicked');
-  location.href = "http://localhost:3000/cow"; 
+  //location.href = "http://localhost:3000/cow"; 
+  document.getElementById("AnimalImage").src ="images/cow.jpg"
 });
 
-function getImage(animal) {
-  switch(animal) {
-    case "camel":
-      myImage = img(src='images/camel.jpg',style='width:500px')
-    break;
-    default:
-      myImage = img(src='images/zoo.jpg',style='width:500px')
-    break;
-  }
-  document.getElementById("animalImage").innerHTML = myImage;
-}
--- web/myapp3/routes/index.js
+++ web/myapp3/routes/index.js
@@ -5,11 +5,10 @@ let index = require('../controllers/index')
 
 /* GET home page. */
 router.get('/',index.index );
-router.get('/zoo',index.index);
+router.get('/zoo',index.zoo);
 router.get('/camel',index.camel);
 router.get('/monkey',index.monkey);
 router.get('/cow',index.cow);
 router.get('/crocodile',index.croc);
-router.post('/clicked', index.clicked);
 
 module.exports = router;
